795ba9d9de513ade7d9af2d741a16fe9f7ad0833
Isaac Lal <isaaclal124@gmail.com>
2023-08-18 01:13:32 -0400

    made deployment work

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect } from 'react';
 import Logo from '../../public/assets/logo.png';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
@@ -36,6 +37,7 @@ const Navbar = () => {
         <Link
           href='https://github.com/lal-isaac/portfolio'
           target='_blank'
+          passHref
         >
           <Image
             src={Logo}
@@ -50,6 +52,7 @@ const Navbar = () => {
               <Link
                 key={nav.id}
                 href={nav.url}
+                passHref
               >
                 <li className='ml-10 text-sm uppercase hover:border-b-2 transition-all border-[#8c52ff] hover:text-[#8c52ff] ease-in duration-100'>
                   {nav.name}
@@ -86,6 +89,7 @@ const Navbar = () => {
               <Link
                 href='https://github.com/lal-isaac/portfolio'
                 target='_blank'
+                passHref
               >
                 <Image
                   src={Logo}
@@ -113,6 +117,7 @@ const Navbar = () => {
                 <Link
                   key={nav.id}
                   href={nav.url}
+                  passHref
                 >
                   <li
                     onClick={() => setNav(false)}
@@ -125,7 +130,7 @@ const Navbar = () => {
             </ul>
             <div className='pt-10 sm:pt-40'>
               <p className='text-sm sm:text-base uppercase tracking-widest text-[#8c52ff]'>
-                Let's Connect
+                Let&#39;s Connect
               </p>
               <div className='text-base sm:text-lg flex items-center justify-between my-4 w-full sm:w-[80%]'>
                 {links.map((link) => (
@@ -134,6 +139,7 @@ const Navbar = () => {
                     href={link.name}
                     target='_blank'
                     rel='noreferrer'
+                    passHref
                   >
                     <div className='rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-100 p-3 cursor-pointer hover:scale-110 hover:shadow-[#8c52ff] hover:text-[#8c52ff] ease-in duration-300'>
                       {link.icon}
