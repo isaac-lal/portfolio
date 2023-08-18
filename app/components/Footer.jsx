dff2f9071ba2c2de5b6bd3ad40c781b5802f7bae
Isaac Lal <isaaclal124@gmail.com>
2023-08-18 01:05:34 -0400

    Added footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='flex flex-col'>
+      <div
+        className='
+        sticky
+             text-xl sm:text-2xl sm:translate-y-52 md:translate-y-80 lg:translate-y-96 text-gray-400 text-center
+             font-bold
+             border-t-4 border-gray-500
+             bottom-0
+             p-4'
+      >
+        © 2023 Isaac Lal
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import About from './components/About';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   );
 }
