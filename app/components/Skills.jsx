905a79a921c6d73412a0c90d249540dc5f29c5a9
Isaac Lal <isaaclal124@gmail.com>
2023-08-22 09:51:36 -0400

    added div to key prop

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -27,24 +27,24 @@ const Skills = () => {
         </h2>
         <div className='text-xs sm:text-sm grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4'>
           {skills.map((skill) => (
-            <div
-              data-aos='zoom-in-up'
-              data-aos-duration='1000'>
+            <div key={skill.id}>
               <div
-                key={skill.id}
-                className='p-6 shadow-lg shadow-gray-400 dark:shadow-gray-100 rounded-xl hover:scale-105 hover:shadow-[#8c52ff] hover:text-[#8c52ff] ease-in duration-300'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto cursor-pointer'>
-                    <Image
-                      src={skill.img}
-                      width='64'
-                      alt='HTML'
-                      data-aos='fade-up'
-                      data-aos-duration='1500'
-                    />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>{skill.name}</h3>
+                data-aos='zoom-in-up'
+                data-aos-duration='1000'>
+                <div className='p-6 shadow-lg shadow-gray-400 dark:shadow-gray-100 rounded-xl hover:scale-105 hover:shadow-[#8c52ff] hover:text-[#8c52ff] ease-in duration-300'>
+                  <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <div className='m-auto cursor-pointer'>
+                      <Image
+                        src={skill.img}
+                        width='64'
+                        alt='HTML'
+                        data-aos='fade-up'
+                        data-aos-duration='1500'
+                      />
+                    </div>
+                    <div className='flex flex-col items-center justify-center'>
+                      <h3>{skill.name}</h3>
+                    </div>
                   </div>
                 </div>
               </div>
