77bf6192e16be37dd523695142532d5f0e7da723
Isaac Lal <isaaclal124@gmail.com>
2023-09-10 15:57:47 -0400

    Updated TypeAnimation description

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -41,15 +41,15 @@ const Home = () => {
             data-aos-once='true'>
             <TypeAnimation
               sequence={[
-                'A Full-Stack Web Developer',
+                'A Web Developer',
+                3500,
+                'A Front-End Developer',
+                3500,
+                'A Back-End Developer',
+                3500,
+                'A Full-Stack Developer',
                 3500,
                 'A Software Engineer',
-                3500,
-                'A Web Designer',
-                3500,
-                'A Coder',
-                3500,
-                'A Programmer',
                 3500,
                 'A Passionate Learner',
                 3500,
