895ae60dbeb1b9eaa015c393c184679dc4d86052
Isaac Lal <isaaclal124@gmail.com>
2025-05-07 15:24:38 -0400

    Removed useEffect from Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import { AiOutlineMail } from 'react-icons/ai';
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
