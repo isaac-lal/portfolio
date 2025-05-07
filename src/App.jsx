7a9cfc1c2d78f640658e5484f5fb57401358c837
Isaac Lal <isaaclal124@gmail.com>
2025-05-07 00:43:03 -0400

    Updated Vite DOM to display components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const Home = () => {
+  return (
+    <div id='home'>
+      <div>
+        <p>
+          Welcome, my name is
+        </p>
+        <h1>Isaac Lal</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
