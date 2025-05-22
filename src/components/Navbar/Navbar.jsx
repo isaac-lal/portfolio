5c9554f7c3a9cdfb602fb4fb4cb49d47c9a98e39
Isaac Lal <isaaclal124@gmail.com>
2025-05-22 12:57:37 -0400

    Added shadow to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { navs } from '../data';
 import './Navbar.css';
-import { navs } from "../data"
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [shadow, setShadow] = useState(false);
 
   const toggleMenu = () => setMenuOpen((prev) => !prev);
   const closeMenu = () => setMenuOpen(false);
 
-  return (
-    <header className={menuOpen ? 'sticky' : ''}>
-      <div className='Navbar__Wrapper'>
-        <img
-          src='assets/logo.png'
-          alt=''
-          width={75}
-          height={75}
-        />
-        <ul className={`Navbar__List ${menuOpen ? 'active' : ''}`}>
-          {navs.map((nav) => (
-            <li>
-              <a
-                key={nav.id}
-                href={nav.url}
-                className='Navbar__Links'
-                onClick={closeMenu}>
-                {nav.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+  useEffect(() => {
+    const handleShadow = () => {
+      setShadow(window.scrollY > 50);
+    };
 
-        <div
-          className={`Navbar__Hamburger ${menuOpen ? 'active' : ''}`}
-          onClick={toggleMenu}>
-          <span className='Navbar__Bar'></span>
-          <span className='Navbar__Bar'></span>
-          <span className='Navbar__Bar'></span>
+    window.addEventListener('scroll', handleShadow);
+  }, []);
+
+  return (
+    <header className={shadow ? 'Navbar__Shadow' : ''}>
+      <div className={menuOpen ? 'sticky' : ''}>
+        <div className='Navbar__Wrapper'>
+          <img
+            src='/assets/logo.png'
+            alt=''
+            width={75}
+            height={75}
+          />
+          <ul
+            className={`Navbar__List ${
+              menuOpen ? 'active Navbar__Shadow' : ''
+            }`}>
+            {navs.map((nav) => (
+              <li>
+                <a
+                  key={nav.id}
+                  href={nav.url}
+                  className='Navbar__Links'
+                  onClick={closeMenu}>
+                  {nav.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div
+            className={`Navbar__Hamburger ${menuOpen ? 'active' : ''}`}
+            onClick={toggleMenu}>
+            <span className='Navbar__Bar'></span>
+            <span className='Navbar__Bar'></span>
+            <span className='Navbar__Bar'></span>
+          </div>
         </div>
       </div>
     </header>
