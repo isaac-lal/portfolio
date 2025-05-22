fca4a9d42c32121a80546c547067a543ffaf6fae
Isaac Lal <isaaclal124@gmail.com>
2025-05-22 14:05:07 -0400

    Removed home description and added padding to links

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
-import { AiOutlineMail } from 'react-icons/ai';
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
-import { BsFillPersonFill } from 'react-icons/bs';
+import { links } from '../data'
 import './Home.css';
 
 const Home = () => {
@@ -37,46 +35,23 @@ const Home = () => {
             style={{ fontSize: '1.4em' }}
           />
         </h2>
-        <p className='Home__Description'>
-          Here is my portfolio, displaying all of my expertise as a Web
-          Developer with full-stack experience. This is where you&#39;ll
-          discover my skills in relevant technologies, my best projects, and a
-          contact form for getting in touch with me. I hope you will enjoy!
-        </p>
         <p className='Home__Connect'>Let's Connect</p>
         <div className='Home__Links'>
+          {links.map((link) => (
+           <div className='home-spacing'>
           <a
-            href='https://www.linkedin.com/in/isaac-lal/'
-            target='_blank'
-            rel='noreferrer'>
+            key={link.id}
+                href={link.name}
+                target='_blank'
+                rel='noreferrer'>
+           
             <div className='Home__LinkIcons'>
-              <FaLinkedinIn />
+              {link.icon}
             </div>
+          
           </a>
-          <a
-            href='https://github.com/isaac-lal'
-            target='_blank'
-            rel='noreferrer'>
-            <div className='Home__LinkIcons'>
-              <FaGithub />
-            </div>
-          </a>
-          <a
-            href='https://drive.google.com/file/d/1gFVD45ODOElkMoLkal-TSXvTorwp0IJH/view?usp=sharing'
-            target='_blank'
-            rel='noreferrer'>
-            <div className='Home__LinkIcons'>
-              <BsFillPersonFill />
-            </div>
-          </a>
-          <a
-            href='mailto:isaaclal124@gmail.com'
-            target='_blank'
-            rel='noreferrer'>
-            <div className='Home__LinkIcons'>
-              <AiOutlineMail />
-            </div>
-          </a>
+          </div>
+          ))}
         </div>
       </div>
     </div>
