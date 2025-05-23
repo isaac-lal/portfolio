b2208e6be2f5d83ebf0c6f5e9b33c07f7f6e91dd
Isaac Lal <isaaclal124@gmail.com>
2025-05-23 14:31:49 -0400

    Built out project component, focusing on exact styling later

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from 'react';
+import { projects } from '../data';
+import { FiExternalLink } from 'react-icons/fi';
+import { AiFillGithub } from 'react-icons/ai';
+import './Projects.css';
 
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id='projects' className='projects-section'>
+      <div className='projects-container'>
+        <h6 className='projects-section-subtitle'>Projects</h6>
+        <h2 className='projects-section-title'>What I&#39;ve Built</h2>
+        <div className='projects-grid'>
+          {projects.map((project) => (
+            <div key={project.id} className='project-card-wrapper'>
+              <div className='project-card'>
+                <img
+                  className='project-image'
+                  src={project.img}
+                  alt={project.name || 'Project screenshot'}
+                />
+                <h2 className='project-name'>{project.name}</h2>
+                <div className='project-links-container'>
+                  <div className='project-link-wrapper'>
+                    <a
+                      href={project.demoURL}
+                      target='_blank'
+                      rel='noreferrer'
+                      className='project-link-button'
+                    >
+                      <div className='project-link-content'>
+                        <p className='project-link-text'>Demo</p>
+                        <div className='project-link-icon'>
+                          <FiExternalLink size={17} />
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div className='project-link-wrapper'>
+                    <a
+                      href={project.codeURL}
+                      target='_blank'
+                      rel='noreferrer'
+                      className='project-link-button'
+                    >
+                      <div className='project-link-content'>
+                        <p className='project-link-text'>Code</p>
+                        <div className='project-link-icon'>
+                          <AiFillGithub size={20} />
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
