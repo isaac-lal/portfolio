f497fa51f7691991889bfc458bfa5cc88b66f692
Isaac Lal <isaaclal124@gmail.com>
2025-09-12 10:32:44 -0400

    rafce for component files

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DarkMode = () => {
+  return (
+    <div>DarkMode</div>
+  )
+}
+
+export default DarkMode
---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StarBackground = () => {
+  return (
+    <div>StarBackground</div>
+  )
+}
+
+export default StarBackground
