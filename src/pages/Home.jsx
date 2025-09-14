14df2f13af9eadf4fe68415d54530260a52ebf7f
Isaac Lal <isaaclal124@gmail.com>
2025-09-14 11:04:50 -0400

    Resolved imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import { Navbar } from "../components/Navbar";
-import { StarBackground } from "@/components/StarBackground";
-import { Hero } from "../components/Hero";
-import { About } from "../components/About";
-import { Skills } from "../components/Skills";
-import { Projects } from "../components/Projects";
-import { Contact } from "../components/Contact";
+import Navbar from "../components/Navbar";
+import StarBackground from "@/components/StarBackground";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 export const Home = () => {
   return (
