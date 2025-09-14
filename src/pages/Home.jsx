7a4578ceabd2a79d58be613d99663b2603fdcf4d
Isaac Lal <isaaclal124@gmail.com>
2025-09-14 10:58:42 -0400

    Resolved Home.jsx imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { About } from "../components/About";
 import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
-import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
@@ -23,9 +22,6 @@ export const Home = () => {
         <Projects />
         <Contact />
       </main>
-
-      {/* Footer */}
-      <Footer />
     </div>
   );
 };
